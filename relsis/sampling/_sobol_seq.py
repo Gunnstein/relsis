7cd2045f64d811fee65756b3526bdd7549c32a42
Gunnstein Thomas Frøseth <gunnstein@mailbox.org>
2020-02-01 14:21:54 +0100

    Added license text and copyright to sampling/_sobol_seq.py

--- relsis/sampling/_sobol_seq.py
+++ relsis/sampling/_sobol_seq.py
@@ -1,6 +1,26 @@
 """
   Licensing:
-    This code is distributed under the MIT license.
+    The MIT License (MIT)
+
+    Copyright (c) 2016 John Burkardt
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
 
   Authors:
     Original FORTRAN77 version of i4_sobol by Bennett Fox.
@@ -8,6 +28,7 @@
     PYTHON version by Corrado Chisari
 
     Original Python version of is_prime by Corrado Chisari
+    Original Python version of get_sample_sobol by Gunnstein T. Frøseth
 
     Original MATLAB versions of other functions by John Burkardt.
     PYTHON versions by Corrado Chisari
